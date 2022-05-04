dbed2c94dd75b73164c1be016f12162be73274c6
Argiris Anastasakis <anastasakis.an@gmail.com>
2022-05-04 10:58:12 +0300

    Add ability to upload post images

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
   has_many :hashtags, dependent: :destroy
   accepts_nested_attributes_for :hashtags, 
                                 allow_destroy: true
+  
+  mount_uploader :image, ImageUploader
 end
