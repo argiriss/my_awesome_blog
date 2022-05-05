e22dfc80d5d5b231e9173911fe20d83e98099781
Argiris Anastasakis <anastasakis.an@gmail.com>
2022-05-05 17:41:11 +0300

    Authenticate users for post methdods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
   before_action :create_new_hashtags, only: [:create, :update]
+  before_action :authenticate_user!, only: %i[ new create destroy ]
 
   def index
     @posts = Post.all.includes(:user, :hashtags, :taggings)
