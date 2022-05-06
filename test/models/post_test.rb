1cf23aac502ec96fbbfb85d90d3fb6f0a3cf03d4
Argiris Anastasakis <anastasakis.an@gmail.com>
2022-05-06 14:38:24 +0300

    Create some tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid post" do
+    post = Post.new(title: "Test", body: "Test", image: "test.png", user: User.default)
+    #assert post.valid?
+  end
+
+  test "invalid without title" do
+    post = Post.new(body: "Test", image: "test.png")
+    refute post.valid?, "post is valid without a name"
+    assert_not_nil post.errors[:name], "no validation error for name present"
+  end
+
+  test "invalid without body" do
+    post = Post.new(title: "Test", image: "test.png")
+    refute post.valid?
+    assert_not_nil post.errors[:email]
+  end
+
+  test "invalid without image" do
+    post = Post.new(title: "Test", body: "Test")
+    refute post.valid?
+    assert_not_nil post.errors[:email]
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid user" do
+    user = User.new(name: "Test", email: "test_1@test.com", image: "test.png")
+    assert user.valid?
+  end
+
+  test "invalid without name" do
+    user = User.new(email: "test_2@test.com", image: "test.png")
+    refute user.valid?, "user is valid without a name"
+    assert_not_nil user.errors[:name], "no validation error for name present"
+  end
+
+  test "invalid without email" do
+    user = User.new(name: "Test", image: "test.png")
+    refute user.valid?
+    assert_not_nil user.errors[:email]
+  end
+
+  test "invalid without image" do
+    user = User.new(name: "Test", email: "test_3@test.com")
+    refute user.valid?
+    assert_not_nil user.errors[:email]
+  end
 end
