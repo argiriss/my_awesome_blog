9e0e6bf9822af76fb40e7517cef652ec54c80758
Argiris Anastasakis <anastasakis.an@gmail.com>
2022-05-06 12:44:09 +0300

    Remove unused code

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,5 +1,5 @@
 class HashtagsController < ApplicationController
-  before_action :set_hashtag, only: %i[ show destroy ]
+  before_action :set_hashtag, only: %i[ destroy ]
 
   def index
     @hashtags = Hashtag.all.includes(:taggings)
@@ -12,33 +12,18 @@ class HashtagsController < ApplicationController
   def create
     @hashtag = Hashtag.new(hashtag_params)
 
-    respond_to do |format|
-      if @hashtag.save
-        format.html { redirect_to hashtag_url(@hashtag), notice: "Hashtag was successfully created." }
-        format.json { render :show, status: :created, location: @hashtag }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @hashtag.errors, status: :unprocessable_entity }
-      end
-    end
+    @hashtag.save
   end
 
   def destroy
     @hashtag.destroy
-
-    respond_to do |format|
-      format.html { redirect_to hashtags_url, notice: "Hashtag was successfully destroyed." }
-      format.json { head :no_content }
-    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_hashtag
       @hashtag = Hashtag.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def hashtag_params
       params.require(:hashtag).permit(:title, :post_id)
     end
