cb08db7626edb6526120eeb312bb82a6a792707b
Argiris Anastasakis <anastasakis.an@gmail.com>
2022-05-06 14:32:08 +0300

    Remove unused code

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :taggings
   has_many :hashtags, through: :taggings
-  accepts_nested_attributes_for :hashtags, 
-                                allow_destroy: true
   
   mount_uploader :image, ImageUploader
 
